0b505a120164d92bf01d6e3bdf4a66996d332e68
NProof <34260623+NProof@users.noreply.github.com>
2018-01-14 15:39:01 +0800

    .h and .cpp

---
+++ Test.cpp
@@ -0,0 +1,12 @@
+// #include <iostream>
+// #include <cstdio>
+#include "main.h"
+
+int main (int argc, char** argv)
+{
+#if defined (LOCAL)
+	freopen("run.out", "w", stdout);
+#endif
+	
+	return 0;
+}
---
+++ main.h
@@ -0,0 +1,26 @@
+// #include <iostream>
+// #include <cstdio>
+// #include <bitset>
+#include <array>
+#include <vector>
+#include <set>
+// #include <ctime>
+
+// using namespace std;
+
+class Broad{
+public :
+	
+};
+
+class Nonogram{
+public :
+	Nonogram(std::array<std::vector<int>, 25> ConditionsOfRow, std::array<std::vector<int>, 25> ConditionsOfCol){
+	}
+	
+	std::set<Broad> solve(Broad initBroad = Broad()){
+		std::set<Broad> ans;
+		
+		return ans;
+	}
+};
