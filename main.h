8cdd7465c3ef3a2c538185f023e5a4d205c30e0b
NProof <34260623+NProof@users.noreply.github.com>
2018-01-17 17:51:21 +0800

    possible()

--- main.h
+++ main.h
@@ -21,6 +21,8 @@ public :
 };
 
 class Nonogram{
+	std::array<std::set<std::bitset<25>, compareBitset>, 25> *possibleSetArrayOfRow;
+	std::array<std::set<std::bitset<25>, compareBitset>, 25> *possibleSetArrayOfCol;
 public :
 	Nonogram(std::array<std::vector<int>, 25> ConditionsOfRow, std::array<std::vector<int>, 25> ConditionsOfCol){
 		
@@ -28,5 +30,41 @@ public :
 	
 	std::set<Broad, Broad::compare> solve(){
 		return std::set<Broad, Broad::compare>();
+	}
+	
+	std::set<std::bitset<25>, compareBitset> possible(std::vector<int> ns){
+		std::set<std::bitset<25>, compareBitset> resultSet;
+		int n = ns.size();
+		if(n==0)
+			return resultSet;
+		int sum = 0;
+		for(int i=0; i<n; i++)
+		sum += ns[i];
+		int n0 = 25-sum+1;
+		for(int i=0; i<(1<<n0); i++){
+			std::bitset<25> bitCombition(i);
+			if(bitCombition.count()==n){
+				int comb = 0;
+				int j=0;
+				while(!bitCombition[j++])
+					comb++;
+				int addBlock[n]{comb}, indexAB = 0;
+				comb += ns[indexAB++]+1;
+				for(; j<n0; j++){
+					if(bitCombition[j]){
+						addBlock[indexAB] = comb;
+						comb += ns[indexAB]+1;
+						indexAB++;
+					}
+					else comb++;
+				}
+				bitCombition.reset();
+				for(j=0; j<n; j++){
+					bitCombition |= ((1<<ns[j])-1)<<(25-addBlock[j]-ns[j]);
+				}
+				resultSet.insert(bitCombition);
+			}
+		}
+		return resultSet;
 	}
 };
