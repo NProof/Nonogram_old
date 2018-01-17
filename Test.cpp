edafaad887e36623bbfa88b911c53feaa9b9f74b
NProof <34260623+NProof@users.noreply.github.com>
2018-01-18 00:10:01 +0800

    update

--- Test.cpp
+++ Test.cpp
@@ -4,35 +4,35 @@
 
 using namespace std;
 
-// void case1(){
-	// std::array<std::vector<int>, 25> rows;
-	// std::array<std::vector<int>, 25> cols;
-	// Nonogram *nonogram = new Nonogram(rows, cols);
-	// std::vector<int> ns;
-	// ns.push_back(1);
-	// ns.push_back(12);
-	// std::set<std::bitset<25>, compareBitset> ans = nonogram->possible(ns);
-	// nonogram->possible(ns);
-	// for(std::set<std::bitset<25>, compareBitset>::iterator it=ans.begin(); it!=ans.end(); ++it){
-		// cout << *it << endl;
-	// }
-	// cout <<  " : " << nonogram->possible(ns).size() << endl;
-// }
+void case1(){
+	std::array<std::vector<int>, 25> rows;
+	std::array<std::vector<int>, 25> cols;
+	Nonogram *nonogram = new Nonogram(rows, cols);
+	std::vector<int> ns;
+	ns.push_back(1);
+	ns.push_back(12);
+	std::set<std::bitset<25>, compareBitset> ans = nonogram->possible(ns);
+	nonogram->possible(ns);
+	for(std::set<std::bitset<25>, compareBitset>::iterator it=ans.begin(); it!=ans.end(); ++it){
+		cout << *it << endl;
+	}
+	cout <<  " : " << nonogram->possible(ns).size() << endl;
+}
 
-// void case2(){
-	// Broad broad;
-	// broad.write(1,2,1);
-	// broad.write(2,2,0);
-	// cout << broad << endl;
-	// broad.clean(2,2);
-	// cout << broad << endl;
-// }
+void case2(){
+	Broad broad;
+	broad.write(1,2,1);
+	broad.write(2,2,0);
+	cout << broad << endl;
+	broad.clean(2,2);
+	cout << broad << endl;
+}
 
-// void clonetest(){
-	// Broad *broad1 = new Broad();
-	// cout << broad1 << endl;
-	// cout << new Broad(*broad1) << endl;
-// }
+void clonetest(){
+	Broad *broad1 = new Broad();
+	cout << broad1 << endl;
+	cout << new Broad(*broad1) << endl;
+}
 
 void getLine(){
 	Broad broad = Broad();
@@ -49,6 +49,9 @@ int main (int argc, char** argv)
 #if defined (LOCAL)
 	freopen("run.out", "w", stdout);
 #endif
-	getLine();
+	// case1();
+	// case2();
+	// clonetest();
+	// getLine();
 	return 0;
 }
