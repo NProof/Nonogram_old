cfae4fe06688afc2ec40670451aa389d7890a234
NProof <34260623+NProof@users.noreply.github.com>
2018-01-17 17:03:03 +0800

    remove

--- main.h
+++ main.h
@@ -1,19 +1,3 @@
-// #include <iostream>
-// #include <cstdio>
-#include <bitset>
-#include <array>
-#include <stack>
-#include <vector>
-#include <set>
-#include <map>
-
-using namespace std;
-
-struct compareBitset {
-	bool operator()(const std::bitset<25>& lhs, const std::bitset<25>& rhs) const { 
-		return lhs.to_ulong() > rhs.to_ulong();
-	}
-};
 
 class Nonogram{
 public :
